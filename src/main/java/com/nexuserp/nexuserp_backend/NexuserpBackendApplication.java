a7ac131099fd3251c1f9f50552a4eeb8084f74d6
Abhijeetr7 <abhijeetrs877@gmail.com>
2026-06-01 13:00:16 +0530

    Initial Spring Boot Setup

---
+++ src/main/java/com/nexuserp/nexuserp_backend/NexuserpBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nexuserp.nexuserp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexuserpBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexuserpBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nexuserp/nexuserp_backend/NexuserpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexuserp.nexuserp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexuserpBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
